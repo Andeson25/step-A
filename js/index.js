461186afc48313cbcaa616728346c1d6b738e34a
kek <oleh.petryk@gmail.com>
2018-08-22 19:46:34 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,91 @@
-let int = null;
-let timerVal = 0;
-let func = () => {
-    timerVal++;
-    document.getElementsByClassName('inner')[0].firstElementChild.innerHTML = timerVal
+$(document).ready(() => {
+    let int = null;
+    let timerVal = 0;
+    let func = () => {
+        timerVal++;
+        $('.inner')[0].firstElementChild.innerHTML = timerVal
 
-    ;
-    if (timerVal === 10) {
-        clearInterval(int);
+        ;
+        if (timerVal === 10) {
+            clearInterval(int);
+        }
+    };
+    int = setInterval(func, 100);
+
+
+    let buttons = $('.bottom-button-number');
+    let labels = $('.approaching-customers-bottom-block-bottom-text-inner');
+    let arrows = [];
+    arrows.push($('.approaching-arrow-up'), $('.approaching-arrow-down'));
+    let currentIndex = 0;
+    buttons[0].classList.add('approach-active');
+    labels[0].classList.add('approach-show');
+    arrows[0].hide();
+
+    for (let i = 0; i < arrows.length; i++) {
+        arrows[i].on('click', (e) => {
+            if (i === 1) {
+                buttons[currentIndex].classList.remove('approach-active');
+                labels[currentIndex].classList.remove('approach-show');
+                buttons[currentIndex + 1].classList.add('approach-active');
+                labels[currentIndex + 1].classList.add('approach-show');
+                currentIndex++;
+            }
+            if (i === 0) {
+                buttons[currentIndex].classList.remove('approach-active');
+                labels[currentIndex].classList.remove('approach-show');
+                buttons[currentIndex - 1].classList.add('approach-active');
+                labels[currentIndex - 1].classList.add('approach-show');
+                currentIndex--;
+            }
+            if (currentIndex === 0) {
+                $('.approaching-arrow-up').hide();
+                $('.approaching-arrow-down').show();
+            }
+            if (currentIndex === 3) {
+                $('.approaching-arrow-up').show();
+                $('.approaching-arrow-down').hide();
+            }
+            if (currentIndex != 3 && currentIndex != 0) {
+                $('.approaching-arrow-up').show();
+                $('.approaching-arrow-down').show();
+            }
+        });
     }
-};
-int = setInterval(func, 100);
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = () => {
+            buttons[currentIndex].classList.remove('approach-active');
+            labels[currentIndex].classList.remove('approach-show');
+            buttons[i].classList.add('approach-active');
+            labels[i].classList.add('approach-show');
+            currentIndex = i;
+            if (currentIndex === 0) {
+                $('.approaching-arrow-up').hide();
+                $('.approaching-arrow-down').show();
+            }
+            if (currentIndex === 3) {
+                $('.approaching-arrow-up').show();
+                $('.approaching-arrow-down').hide();
+            }
+            if (currentIndex != 3 && currentIndex != 0) {
+                $('.approaching-arrow-up').show();
+                $('.approaching-arrow-down').show();
+            }
+        }
+    }
+
+    let leftSteps = $('.steps-left .step-box .step-hidden-content');
+    let rightSteps = $('.steps-right .step-box .step-hidden-content');
+
+    for (let i = 0; i < 3; i++) {
+        leftSteps[i].onclick = (e) => {
+            leftSteps[i].style.opacity = '0';
+            rightSteps[i].style.opacity = '0';
+        }
+        rightSteps[i].onclick = (e) => {
+            leftSteps[i].style.opacity = '0';
+            rightSteps[i].style.opacity = '0';
+        }
+    }
+});
